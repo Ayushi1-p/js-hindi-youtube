89641352b3c631117ee09fb22074a03b7a334223
Ayushi1-p <ayushipundir72@gmail.com>
2025-05-19 05:36:47 +0000

    container confg

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+let accountEmail = "ayuhsi@gmail.com";
+var accountPassword = "2345";
+accountCity = "gzb";
+
+accountEmail = "ahsh@gmail.com";
+accountPassword = 4567;
+accountCity = "jaipur";
+console.table([accountEmail , accountPassword , accountCity]);
